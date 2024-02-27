e7d909319b19335b4c45bd574ca10ebbe7702365
Hayyat Hussain <hayyathussain73@gmail.com>
2024-02-27 22:10:33 +0500

    Functionality Added

--- launch-countdown-timer-main/script.js
+++ launch-countdown-timer-main/script.js
@@ -1,10 +1,37 @@
-const countDownHeadings = document.querySelectorAll("h1");
+const day = document.getElementById("day"),
+  hour = document.getElementById("hour"),
+  min = document.getElementById("min"),
+  sec = document.getElementById("sec");
 
 const countDown = () => {
-  countDownHeadings.forEach((element) => {
-    const newa = Number(element) - 1;
-    element.innerText = newa;
-  });
+  let dayText = Number(day.innerText),
+    hourText = Number(hour.innerText),
+    minText = Number(min.innerText),
+    secText = Number(sec.innerText);
+  const myInterval = setInterval(() => {
+    secText--;
+    if (secText < 0) {
+      secText = 59;
+      minText--;
+      if (minText < 0) {
+        minText = 59;
+        hourText--;
+        if (hourText < 0) {
+          hourText = 23;
+          dayText--;
+          if (dayText < 0) {
+            clearInterval(myInterval);
+          }
+        }
+      }
+    }
+    day.innerText = dayText;
+    hour.innerText = hourText;
+    min.innerText = minText;
+    sec.innerText = secText;
+  }, 1000);
 };
 
-window.onload = setInterval(countDown, 1000);
+window.addEventListener("load", () => {
+  countDown();
+});
